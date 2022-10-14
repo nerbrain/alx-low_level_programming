8eb61a0a9f01bac1f3086882922d181fcc19494e
nerbrain <malikmox@gmail.com>
2022-10-14 21:25:56 +0000

    fixed task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * main - Start Function
  *
- * Return: return 0
+ * Return: return 1
  */
 
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
