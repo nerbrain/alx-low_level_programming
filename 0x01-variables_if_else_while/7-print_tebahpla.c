44a726e62ade2c1acfba97fb3e1a172ad62f86b0
nerbrain <malikmox@gmail.com>
2022-10-16 22:38:01 +0000

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * main - start function
+ * Return: return 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 122; a > 96; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+
+}
