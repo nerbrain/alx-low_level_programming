5d77156ef96123d9871baa8fb85a8bbeceab7a9e
nerbrain <malikmox@gmail.com>
2022-10-16 22:21:39 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - start function
+ * Return: return 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+
+}
