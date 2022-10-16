82d3de9e46d4909845fca3fb6b1b4a04b4bba95f
nerbrain <malikmox@gmail.com>
2022-10-16 20:49:14 +0000

    fixed wording

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - Start function
  *
- * Return - always 0
+ * Return: Return 0
  */
 
 int main(void)
@@ -16,15 +16,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("%d is positive", n);
+		printf("%d is positive ", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf("%d is zero ", n);
 	}
 	else
 	{
-		printf("%d is negative", n);
+		printf("%d is negative ", n);
 	}
 
 	return (0);
