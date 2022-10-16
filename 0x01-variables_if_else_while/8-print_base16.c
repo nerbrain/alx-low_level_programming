f9dcb4cb43e1cef175dba5fc73d047b3307ceffe
nerbrain <malikmox@gmail.com>
2022-10-16 22:41:57 +0000

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * main - start function
+ * Return: return 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	for (a = 97; a < 103; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+
+}
