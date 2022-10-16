a71ac413d9d212e7274d6893869ba47b1f6e34e1
nerbrain <malikmox@gmail.com>
2022-10-16 22:47:58 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * main - start function
+ * Return: return 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');													}
+	}
+	putchar('\n');
+	return (0);
+
+}
