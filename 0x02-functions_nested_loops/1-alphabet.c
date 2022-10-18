44306beee088b336ea3eb1be8d92261f8577afaa
nerbrain <malikmox@gmail.com>
2022-10-18 19:08:40 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - start function
+ *
+ * Return: Return 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		printf("%d\n", a);
+	}
+
+	return (0);
+}
