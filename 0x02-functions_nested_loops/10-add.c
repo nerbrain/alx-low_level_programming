ad82ffc73a907740951c2307b104512188e75e28
nerbrain <malikmox@gmail.com>
2022-10-19 20:53:11 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * add - add two numbers
+ *
+ * @n: value to be added to o
+ * @o: value to be added to n
+ *
+ * Return: return sum of n and o
+ */
+int add(int n, int o)
+{
+	return (n + o);
+}
