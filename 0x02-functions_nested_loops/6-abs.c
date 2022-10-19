443e05e1c99559b9de04fac7b570053415e97f49
nerbrain <malikmox@gmail.com>
2022-10-19 13:12:24 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _abs - checks absolute value
+ *
+ * @n: ACII value to be checked
+ *
+ * Return: something
+ */
+int _abs(int n)
+{
+
+	int p = abs(n);
+
+	return (p);
+}
