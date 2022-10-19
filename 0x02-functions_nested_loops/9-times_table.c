d245498fffff4416a18b7336eabd2da8d29d71fc
nerbrain <malikmox@gmail.com>
2022-10-19 20:17:27 +0000

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * times_table - prints multiples of 9
+ *
+ *
+ * Return: something
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+
+			if(c <= 9)
+			{
+				_putchar(' ');
+				_putchar(c + 48);
+			}
+			else
+			{
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+			}
+
+			if (b == 9){
+				_putchar('$');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
