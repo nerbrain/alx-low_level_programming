76c00029c44780d6b05ce7fddb92d1a5fca78127
nerbrain <malikmox@gmail.com>
2022-10-19 18:48:25 +0000

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * jack_bauer - prints minutes in a day
+ *
+ *
+ * Return: something
+ */
+void jack_bauer(void)
+{
+	int w, x, y, z;
+
+	for (w = 48; w <= 50; w++)
+	{
+		for (x = 48; x <= 57; x++)
+		{
+			for (y = 48; y <= 53; y++)
+			{
+				for (z = 48; z <= 57; z++)
+				{
+					if (w >= 50 && x >= 52)
+					{
+						break;
+					}
+
+					_putchar(w);
+					_putchar(x);
+					_putchar(58);
+					_putchar(y);
+					_putchar(z);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
