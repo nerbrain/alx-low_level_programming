c5ad17d0c3da8716e3b28caa2ecac611071e07fc
nerbrain <malikmox@gmail.com>
2022-10-21 21:19:04 +0000

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a line n times
+ *
+ * @n: number of times to print \
+ *
+ * Return: N/A
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(' ');
+			}
+
+			_putchar(92);
+			_putchar('\n');
+		}
+
+		_putchar('\n');
+
+	}
+}
