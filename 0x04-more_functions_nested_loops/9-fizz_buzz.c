7ae872cd2ca52ad6ed83d0a3bb34532c16214150
nerbrain <malikmox@gmail.com>
2022-10-21 22:48:21 +0000

    fixed task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,16 @@ int main(void)
 	for (a = 1; a <= 100; a++)
 	{
 		if ((a % 3 == 0) && (a % 5 == 0))
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (a % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (a % 5 == 0)
-			printf("Buzz ");
-		else if (a != 100)
-			printf("%d ", a);
+			printf("Buzz");
 		else
 			printf("%d", a);
+
+		if (a != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
