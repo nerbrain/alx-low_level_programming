d7b8e136bfe221184428c5bfc063be2ef328abe0
nerbrain <malikmox@gmail.com>
2022-10-21 15:19:07 +0000

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers starting from 0 - 9
+ *
+ * Return: return success
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+
+	_putchar('\n');
+}
