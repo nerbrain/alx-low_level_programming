77dda2c658e136b2567e22d1249187caa531610f
nerbrain <malikmox@gmail.com>
2022-10-21 14:54:21 +0000

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * mul- multiply two numbers
+ *
+ * @a : number to be multiplied to b
+ * @b : number to be multiplied to a
+ *
+ * Return: returns the ans
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
