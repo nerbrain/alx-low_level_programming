125b398544cc2441d2417ff630de194d4d7cd7b7
nerbrain <malikmox@gmail.com>
2022-10-26 20:34:52 +0000

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include<string.h>
+/**
+ * print_rev - print string in reverse
+ *
+ *@s: string
+ *Return: n/a
+ */
+
+void print_rev(char *s)
+{
+	int i = strlen(s);
+
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
