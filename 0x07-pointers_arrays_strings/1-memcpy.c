86efdd73c4c3550f344181d2e63b6d947efe1505
nerbrain <malikmox@gmail.com>
2022-11-02 20:29:00 +0000

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies 
+ * @dest: copy to
+ * @src: copy from
+ * @n: how many times
+ * Return: return value
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
