e267ede05a29484444fef3af19a195f54893882d
nerbrain <malikmox@gmail.com>
2022-11-02 20:59:34 +0000

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - locates character
+ * @s: string to find char
+ * @c: char to find
+ * Return: n/a
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; s[a] != '/0'; a++)
+	{
+		if (s[a] == c)
+		{
+			return (s + a);
+		}
+	}
+	if (s[a] == c)
+		return (s + i);
+
+	return (0);
+}
