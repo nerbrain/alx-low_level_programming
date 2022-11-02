b90377d51bef80fb246649646d3631803fb4f56e
nerbrain <malikmox@gmail.com>
2022-11-02 20:31:43 +0000

    fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int a;
+	unsigned int a;
 
 	for (a = 0; a < n; a++)
 	{
-		dest[i] = src[i];
+		dest[a] = src[a];
 	}
 	return (dest);
 }
