c4f423d42c41f3b3217d760c421e88353ee3ffbf
nerbrain <malikmox@gmail.com>
2022-11-03 21:10:34 +0000

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *is_prime - is prime?
+ *@a: int
+ *@n: int
+ *
+ *Return: int
+ */
+
+int is_prime(int a, int n)
+{
+	if (n < 2 || !(n % a))
+		return (0);
+	else if (i > n / 2)
+		return (1);
+	else
+		return (is_prime(a + 1, n));
+}
+
+/**
+ *is_prime_number - check for prime
+ *@n: int
+ *
+ *Return: integer
+ */
+
+int is_prime_number(int n)
+{
+	return (is_prime(2, n));
+}
