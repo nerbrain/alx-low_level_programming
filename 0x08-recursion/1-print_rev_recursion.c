48ab3d12fa342c62dc075d207302c497dc76f209
nerbrain <malikmox@gmail.com>
2022-11-03 09:31:12 +0000

    recursion string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print in reverse
+ *
+ * @s: string
+ *
+ * Return: n/a
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
