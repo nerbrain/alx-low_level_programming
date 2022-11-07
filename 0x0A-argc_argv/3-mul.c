f196495b51cfe5179880e9e65fc437d632ddba8a
nerbrain <malikmox@gmail.com>
2022-11-07 11:53:40 +0000

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiply arguments
+ *
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	if ( argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
