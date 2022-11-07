04ac0a477038f9c022bf38cb27f13c32550248e1
nerbrain <malikmox@gmail.com>
2022-11-07 11:17:48 +0000

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print name
+ *
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * Return: 0
+ */
+
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
