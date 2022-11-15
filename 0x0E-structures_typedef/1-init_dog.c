57958678a8a54f036bc7a7622b3bc9ba10726da1
nerbrain <malikmox@gmail.com>
2022-11-15 12:08:42 +0000

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Initialize type dog
+ * @d: the dog
+ * @name: name of the dog
+ * @age: dog's age
+ * @owner: the owner of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
