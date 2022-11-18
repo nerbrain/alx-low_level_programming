b2464935c062be04fba9786c42f9482b5f04aa9d
nerbrain <malikmox@gmail.com>
2022-11-18 13:07:11 +0000

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum 
+ * @n: number of args
+ *
+ * Return: sum of inputs
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int a = 0;
+	int sum = 0;
+
+	if (a == 0)
+		return (0);
+
+	va_start(list, n);
+
+	for (a = 0; a < n; a++)
+		sum += va_arg(list, int);
+
+	va_end(list);
+
+	return(sum);
+}
